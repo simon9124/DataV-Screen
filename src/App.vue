ac7589d8ef4f1410902cb63e47d91d50e77ac807
simon9124 <435776313@qq.com>
2021-02-24 19:00:28 +0800

    布局更新
    Signed-off-by: simon9124 <435776313@qq.com>

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <template>
-  <div id="app">
-    <Screen />
+  <div id="app"
+       class="layout">
+    <Layout>
+      <Sider hide-trigger>
+        <side-menu></side-menu>
+      </Sider>
+      <Layout>
+        <Content style="background:#fff;height:calc(100vh)">
+          <screen></screen>
+        </Content>
+      </Layout>
+    </Layout>
   </div>
 </template>
 
 <script>
+import SideMenu from './components/SideMenu.vue'
 import Screen from './components/Screen.vue'
 
 export default {
   name: 'App',
   components: {
+    SideMenu,
     Screen
   }
 }
@@ -20,7 +32,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  // text-align: center;
+  // color: #2c3e50;
 }
 </style>
