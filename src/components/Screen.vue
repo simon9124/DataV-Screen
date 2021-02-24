d184f123894cebd785b3130ea230cda70bb0bfa6
simon9124 <435776313@qq.com>
2021-02-24 18:04:45 +0800

    临时
    Signed-off-by: simon9124 <435776313@qq.com>

---
+++ src/components/Screen.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Button>Default</Button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Screen',
+
+}
+</script>
+
+<style scoped lang="scss">
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// iview
+import ViewUI from 'view-design'
+import 'view-design/dist/styles/iview.css'
+Vue.use(ViewUI)
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: (h) => h(App),
 }).$mount('#app')
