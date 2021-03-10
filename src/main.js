c73cd33f4f0559f7ae0c8d6900b366198795f52b
simon9124 <435776313@qq.com>
2021-03-10 17:49:06 +0800

    导入data-view
    Signed-off-by: simon9124 <435776313@qq.com>

--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ import ViewUI from 'view-design'
 import 'view-design/dist/styles/iview.css'
 Vue.use(ViewUI)
 
+// datav
+import dataV from '@jiaminghi/data-view'
+Vue.use(dataV)
+
 Vue.config.productionTip = false
 Vue.prototype.$bus = new Vue() // event Bus
 
