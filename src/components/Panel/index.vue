a900459f75d48aa79bc948458e51d6440698fcfa
simon9124 <435776313@qq.com>
2021-03-19 19:04:21 +0800

    右侧面板
    Signed-off-by: simon9124 <435776313@qq.com>

--- src/components/Panel/index.vue
+++ src/components/Panel/index.vue
@@ -2,11 +2,15 @@
   <div id="panel">
     <Form :model="formData"
           label-position="top">
-      <FormItem label="主题背景">
+
+      <!-- 大屏设置 -->
+      <FormItem v-if="elementName==='大屏设置'"
+                label="主题背景">
         <Input v-model="formData.background"
                @on-blur="onBlur"></Input>
       </FormItem>
-      <FormItem label="大屏比例">
+      <FormItem v-if="elementName==='大屏设置'"
+                label="大屏比例">
         <span class="inline-input-label">宽</span>
         <Input v-model="formData.width"
                @on-blur="onBlur"
@@ -16,6 +20,14 @@
                @on-blur="onBlur"
                class="inline-input"></Input>
       </FormItem>
+
+      <!-- 轮播表 -->
+      <FormItem v-if="elementName==='轮播表'"
+                label="数据">
+        <Input v-model="formData.background"
+               @on-blur="onBlur"></Input>
+      </FormItem>
+
     </Form>
   </div>
 </template>
@@ -31,9 +43,15 @@ export default {
   },
   data () {
     return {
-      element: 'screen', // 元素类型
+      elementName: '大屏设置', // 元素类型
       formData: {} // 面板表单数据
     }
+  },
+  mounted () {
+    this.$bus.$on('click-component', obj => {
+      console.log('panel', obj);
+      this.elementName = obj.componentName
+    })
   },
   created () {
     this.formData = JSON.parse(JSON.stringify(this.styleProps))
