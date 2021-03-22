1f419745ffa8c016a2fda9a43a683d730a8b8458
simon9124 <435776313@qq.com>
2021-03-22 16:25:04 +0800

    右侧面板事件更新
    Signed-off-by: simon9124 <435776313@qq.com>

--- src/components/Panel/index.vue
+++ src/components/Panel/index.vue
@@ -7,25 +7,39 @@
       <FormItem v-if="elementName==='大屏设置'"
                 label="主题背景">
         <Input v-model="formData.background"
-               @on-blur="onBlur"></Input>
+               @on-blur="onBlur('大屏设置','background')"></Input>
       </FormItem>
       <FormItem v-if="elementName==='大屏设置'"
                 label="大屏比例">
         <span class="inline-input-label">宽</span>
         <Input v-model="formData.width"
-               @on-blur="onBlur"
+               @on-blur="onBlur('大屏设置','width')"
                class="inline-input"></Input>
         <span class="inline-input-label">高</span>
         <Input v-model="formData.height"
-               @on-blur="onBlur"
+               @on-blur="onBlur('大屏设置','height')"
                class="inline-input"></Input>
       </FormItem>
 
       <!-- 轮播表 -->
       <FormItem v-if="elementName==='轮播表'"
-                label="数据">
-        <Input v-model="formData.componentName"
-               @on-blur="onBlur"></Input>
+                label="表头数据">
+        <div v-for="(item,i) in formData.header"
+             :key="i">
+          <Input :value="item"
+                 @on-blur="onBlur('轮播表','header',i)"></Input>
+        </div>
+      </FormItem>
+      <FormItem v-if="elementName==='轮播表'"
+                label="表数据">
+        <div v-for="(item,i) in formData.data"
+             :key="i">
+          <span v-for="(_item,_i) in item"
+                :key="_i">
+            <Input :value="_item"
+                   @on-blur="onBlur('轮播表','data',i,_i)"></Input>
+          </span>
+        </div>
       </FormItem>
 
       <!-- 排名轮播表 -->
@@ -50,9 +64,10 @@ export default {
     }
   },
   mounted () {
-    // 接收兄弟组件数据，更新右侧面板
+    // 接收兄弟组件事件：点击组件刷新右侧面板
     this.$bus.$on('click-component', obj => {
       console.log('panel', obj);
+      document.getElementsByTagName('input').forEach(ele => { ele.blur() }) // 主动触发所有的inpu框失去焦点
       this.formData = obj.data.config
       this.elementName = obj.componentName
     })
@@ -62,8 +77,14 @@ export default {
     this.formData = JSON.parse(JSON.stringify(this.screenProps))
   },
   methods: {
-    // input框失去焦点，向父组件Sreen和兄弟组件发送事件：更新面板表单数据
-    onBlur () {
+    // input框失去焦点，向父组件Sreen和兄弟组件发送事件：属性面板数据更新
+    onBlur (componentName, type, index, _index) {
+      // console.log(componentName, type, index, _index);
+      if (_index !== undefined) {
+        this.$set(this.formData[type][index], _index, event.target.value)
+      } else if (index !== undefined) {
+        this.$set(this.formData[type], index, event.target.value)
+      }
       this.$bus.$emit('attribute-update', this.formData)
     }
   },
@@ -71,4 +92,5 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@import './index.scss';
 </style>
